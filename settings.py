049d0aead7f5101273eadcc4f1d1d162cd830380
Diego Fernando Campos Alba <111471379+dfcamposalba@users.noreply.github.com>
2022-11-19 18:38:53 -0500

    Se ajustó el nombre del .env

--- settings.py
+++ settings.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 URL = os.environ.get("URL")
-PORT = int(os.environ.get("PORT"))
+PORT = os.environ.get("PORT")
 VOTES_URL = os.environ.get("VOTES_URL")
 SECURITY_URL = os.environ.get("SECURITY_URL")
